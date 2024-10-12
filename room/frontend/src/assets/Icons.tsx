33a3f84d32145e840e1d1269d8de203882493542
Praveen Perumal <ppraveen98841@gmail.com>
2024-10-12 10:32:06 +0530

    Improve room/session-list UI

--- room/frontend/src/assets/Icons.tsx
+++ room/frontend/src/assets/Icons.tsx
@@ -1,42 +1,18 @@
-function HomeIcon() {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z" /></svg>
-    )
-}
-
-function BatchesIcon() {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M0-240v-63q0-43 44-70t116-27q13 0 25 .5t23 2.5q-14 21-21 44t-7 48v65H0Zm240 0v-65q0-32 17.5-58.5T307-410q32-20 76.5-30t96.5-10q53 0 97.5 10t76.5 30q32 20 49 46.5t17 58.5v65H240Zm540 0v-65q0-26-6.5-49T754-397q11-2 22.5-2.5t23.5-.5q72 0 116 26.5t44 70.5v63H780Zm-455-80h311q-10-20-55.5-35T480-370q-55 0-100.5 15T325-320ZM160-440q-33 0-56.5-23.5T80-520q0-34 23.5-57t56.5-23q34 0 57 23t23 57q0 33-23 56.5T160-440Zm640 0q-33 0-56.5-23.5T720-520q0-34 23.5-57t56.5-23q34 0 57 23t23 57q0 33-23 56.5T800-440Zm-320-40q-50 0-85-35t-35-85q0-51 35-85.5t85-34.5q51 0 85.5 34.5T600-600q0 50-34.5 85T480-480Zm0-80q17 0 28.5-11.5T520-600q0-17-11.5-28.5T480-640q-17 0-28.5 11.5T440-600q0 17 11.5 28.5T480-560Zm1 240Zm-1-280Z" /></svg>
-    )
-}
-
 function LogoutIcon() {
     return (
         <svg className="logout-icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M120-120v-720h360v80H200v560h280v80H120Zm520-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" /></svg>
     )
 }
 
-function MemberIcon() {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#000000"><path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z"/></svg>
-    )
-}
-
-function SessionIcon() {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M480-400 40-640l440-240 440 240-440 240Zm0 160L63-467l84-46 333 182 333-182 84 46-417 227Zm0 160L63-307l84-46 333 182 333-182 84 46L480-80Zm0-411 273-149-273-149-273 149 273 149Zm0-149Z"/></svg>
-    )
-}
-
 function HistoryIcon() {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M480-120q-138 0-240.5-91.5T122-440h82q14 104 92.5 172T480-200q117 0 198.5-81.5T760-480q0-117-81.5-198.5T480-760q-69 0-129 32t-101 88h110v80H120v-240h80v94q51-64 124.5-99T480-840q75 0 140.5 28.5t114 77q48.5 48.5 77 114T840-480q0 75-28.5 140.5t-77 114q-48.5 48.5-114 77T480-120Zm112-192L440-464v-216h80v184l128 128-56 56Z"/></svg>
+        <svg className="history-icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M480-120q-138 0-240.5-91.5T122-440h82q14 104 92.5 172T480-200q117 0 198.5-81.5T760-480q0-117-81.5-198.5T480-760q-69 0-129 32t-101 88h110v80H120v-240h80v94q51-64 124.5-99T480-840q75 0 140.5 28.5t114 77q48.5 48.5 77 114T840-480q0 75-28.5 140.5t-77 114q-48.5 48.5-114 77T480-120Zm112-192L440-464v-216h80v184l128 128-56 56Z"/></svg>
     )
 }
 
 function OnGoingIcon() {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="m612-292 56-56-148-148v-184h-80v216l172 172ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-400Zm0 320q133 0 226.5-93.5T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160Z"/></svg>
+        <svg className="ongoing-icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="m612-292 56-56-148-148v-184h-80v216l172 172ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-400Zm0 320q133 0 226.5-93.5T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160Z"/></svg>
     )
 }
 
@@ -52,4 +28,10 @@ function RoomIcon() {
     )
 }
 
-export { RoomIcon, HomeIcon, BatchesIcon, LogoutIcon, MemberIcon, SessionIcon, HistoryIcon, OnGoingIcon, RefreshIcon };
+function TrashIcon({onClick}: {onClick: () => void;}) {
+    return (
+        <svg onClick={onClick} className="trash-icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M200-120v-600h-40v-80h200v-40h240v40h200v80h-40v600H200Zm80-80h400v-520H280v520Zm80-80h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+    )
+}
+
+export { LogoutIcon, HistoryIcon, OnGoingIcon, RefreshIcon, RoomIcon, TrashIcon };
--- room/frontend/src/components/sessions/SessionList.tsx
+++ room/frontend/src/components/sessions/SessionList.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { HistoryIcon, OnGoingIcon } from "../../assets/Icons";
+import { TrashIcon } from "../../assets/Icons";
 import { SessionListType } from "./Sessions";
 
 function SessionList({ sessions, onCreate, onView }: { sessions: SessionListType, onCreate: () => void, onView: (_id: string) => void }) {
@@ -8,44 +8,39 @@ function SessionList({ sessions, onCreate, onView }: { sessions: SessionListType
   const filteredSessions = sessions.history.filter(session => session.timestamp.toLowerCase().includes(searchTerm.toLowerCase())).sort().reverse();
   const filteredSessionsError = sessions.history.length == 0 ? "No previous sessions available..." : "No match found..."
   const ongoing = sessions.onGoing !== null;
-  
-  const handleCreate = () => {
-    if (ongoing) return;
-    onCreate();
-  }
 
   return (
-    <>
-      <div className="header">
-        <input
-          type="text"
-          placeholder="Filter sessions..."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <button className={ongoing ? "new-button disabled" : "new-button"} onClick={handleCreate}>New</button>
+    <div className="room-list">
+      <div className="ongoing">
+        <h2 className="bold">Ongoing: </h2>
+        {ongoing ?
+          <a className="condensed bold badge" onClick={_ => onView(sessions.onGoing!._id)}>
+            {sessions.onGoing!.timestamp}
+          </a>
+          : <p className="condensed badge">None</p>}
       </div>
-      <div className="session-list">
-        <div>
-          <h2><OnGoingIcon/> Ongoing</h2>
-          {ongoing ?
-            <a onClick={_ => onView(sessions.onGoing!._id)}>
-              {sessions.onGoing!.timestamp}
-            </a>
-            : <p>No ongoing session...</p>}
-        </div>
 
-        <div>
-          <h2><HistoryIcon/> History</h2>
-          <ul className="list">
-            {filteredSessions.length > 0 ? filteredSessions.map(session => <li key={session.timestamp}>
-              <a onClick={_ => onView(session._id)}>
-              {session.timestamp}
-              </a></li>) : <p>{filteredSessionsError}</p>}
-          </ul>
+      <div className="history">
+        <div className="filter">
+          <input
+            type="text"
+            placeholder="Filter sessions..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button className="condensed fill" onClick={onCreate} disabled={ongoing}>New</button>
         </div>
+        {filteredSessions.length > 0 ? (
+          <ul>
+            {filteredSessions.map(session => (
+              <li key={session._id}>
+                <a className="bold" onClick={_ => onView(session._id)}>{session.timestamp}</a>
+                <TrashIcon onClick={() => { }} />
+              </li>))}
+          </ul>
+        ) : <p className="condensed">{filteredSessionsError}</p>}
       </div>
-    </>
+    </div>
   )
 }
 
